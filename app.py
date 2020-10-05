8ead74adb6f336c0da71c35bd10d51dc104490e8
ashishsahu1 <ashishsahu1999@hotmail.com>
2020-10-06 01:55:14 +0530

    Added app.py
    app.py is the main python script enabling request calls and flask deployment

---
+++ app.py
@@ -0,0 +1,40 @@
+import requests
+import configparser
+from flask import Flask, render_template, request
+from requests import api
+
+app = Flask(__name__)
+
+@app.route('/')
+def weather_dashboard():
+    return render_template('home.html')
+
+@app.route('/results', methods=['POST'])
+def render_results():
+    cityName = request.form['cityName']
+
+    api= get_api_key()
+    data = get_Weather_resullts(cityName,api)
+    temp = "{0:.2f}".format(data['main']['temp'])
+    feels_like = "{0:.2f}".format(data['main']['feels_like'])
+    weather = data["weather"][0]['main']
+    location = data['name']
+
+    return render_template('results.html',location = location, 
+                            temp = temp,feels_like = feels_like,
+                            weather = weather)
+
+
+def get_api_key():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config['openweathermap']['api']
+def get_Weather_resullts(cityName,api_key):
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&units={}&appid={}".format(cityName,'metric',api_key)
+    r = requests.get(url)
+    return r.json()
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
+    print(get_Weather_resullts("Jamshedpur",get_api_key()))
